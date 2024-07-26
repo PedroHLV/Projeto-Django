0514ea583ca9cb3b243ec29687afeddc5534e264
PedroHLV <pedrolopesv@outlook.com>
2024-07-26 10:12:10 -0300

    feat: new app recipes

---
+++ recipes/urls.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.urls import path
+from django.contrib import admin
+from recipes.views import home, contato, sobre
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato),
+]
