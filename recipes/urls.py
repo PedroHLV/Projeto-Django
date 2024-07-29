fb3606b8f635fb51ea900f8fbf7cecebc5c7b6c7
PedroHLV <pedrolopesv@outlook.com>
2024-07-29 10:11:50 -0300

    feat: new view recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse
 from django.urls import path
 from django.contrib import admin
 from recipes.views import home
+from recipes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipes),
 ]
